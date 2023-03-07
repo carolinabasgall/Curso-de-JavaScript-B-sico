1b42cd09dce402cb14a66d11fd2c5e820b35b2c5
carolinabasgall <carolinabasgall@gmail.com>
2023-03-07 15:58:34 -0300

    ejercicio 2 Una lista con mis datos

---
+++ Ejercicio/ejercicio-2.js
@@ -0,0 +1,13 @@
+const lista = {
+    nombre: "Carolina Basgall",
+    edad: 31,
+    desarrollador: true,
+    fecha: new Date (1992, 1, 7),
+    libro: {
+        titulo: "Jasy",
+        autor: "Florencia Bonelli",
+        fecha: (2014, 7, 2),
+        url: "https://www.cuspide.com/9789870436126/1++Jasy+Trilogia+Del+Perdon",
+    }
+}
+console.log(lista)
