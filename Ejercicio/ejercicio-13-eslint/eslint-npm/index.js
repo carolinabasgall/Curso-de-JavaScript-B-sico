fe4600b774bb14da542c0d74e8e9a2dd979962b9
carolinabasgall <carolinabasgall@gmail.com>
2023-03-18 16:27:54 -0300

    ejercicio 13 - "Configuracion y uso de ESLint"

---
+++ Ejercicio/ejercicio-13-eslint/eslint-npm/index.js
@@ -0,0 +1,12 @@
+// Sesión 4 - Strings (cadenas de caracteres)
+let str_sng = "Hola soy un texto con comillas simples"
+let str_dbl = "Hola soy un texto con comillas dobles"
+
+console.log(str_sng)
+console.log(str_dbl)
+
+let str_comillas = "El otro dia me dijo literalmente 've a sacar la basura'"
+console.log(str_comillas)
+
+// Introduccion de variables en html
+let libros = ["Empieza por el por qué", "El monje que vendió su ferrari", "El poder del ahora"]
